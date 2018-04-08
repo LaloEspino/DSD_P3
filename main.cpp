a4c31123de88bdb56a09ee535a5d67e6a4a4102a
LaloEspino <lalospn@gmail.com>
2018-04-08 02:53:36 -0500

    POO
    Programado orientado a objetos funcionando al 100 con un solo objeto

--- main.cpp
+++ main.cpp
@@ -22,6 +22,66 @@
 
 using namespace std;
 
+class Barra {
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    
+public:
+    void graficarBarra();
+    void setX1(int x);
+    void setY1(int y);
+    void setX2(int x);
+    void setY2(int y);
+    int getX1();
+    int getY1();
+    int getX2();
+    int getY2();
+};
+
+/*  Dibuja la linea correspondiente con los parametros establecidos  */
+
+void Barra::graficarBarra() {
+    gfx_line(x1, y1, x2, y2);
+}
+
+/*  Setters  */
+
+void Barra::setX1(int x) {
+    x1 = x;
+}
+
+void Barra::setY1(int y) {
+    y1 = y;
+}
+
+void Barra::setX2(int x) {
+    x2 = x;
+}
+
+void Barra::setY2(int y) {
+    y2 = y;
+}
+
+/*  Getters  */
+
+int Barra::getX1() {
+    return x1;
+}
+
+int Barra::getY1() {
+    return y1;
+}
+
+int Barra::getX2() {
+    return x2;
+}
+
+int Barra::getY2() {
+    return y2;
+}
+
 int main (int argc, char* argv[]) {
     
     /*  Variables de gfx  */
@@ -32,6 +92,10 @@ int main (int argc, char* argv[]) {
     /*  Variables de negocio  */
     const int BARRAS_DESEADAS   = atoi(argv[1]);
     const int LIMITE_SUPERIOR   = atoi(argv[2]);
+    
+    int xMax = 0;
+    
+    Barra barras;
 
     printf("\n\tBarras: %d\n\tLímite superior: %d\n\n", BARRAS_DESEADAS, LIMITE_SUPERIOR);
 
@@ -39,52 +103,50 @@ int main (int argc, char* argv[]) {
     gfx_open(GFX_WIDTH, GFX_HEIGHT, GFX_TITLE);
     gfx_color(0, 200, 100);
     
-    /*  Variables de barras  */
-
-    int x1 = 0;
-    int y1 = 0;
-    int x2 = 0;
-    int y2 = 0;
-    int xMax = 0;
-
     char exit; /*  Variable para fin de ejecución de programa  */
     
     srand (time(NULL)); /*  Random seed  */
 
     for (int j = 0; j < BARRAS_DESEADAS; j ++) { /*  Se ejecuta por el número deseado de barras  */
-      
+
         xMax = rand() % LIMITE_SUPERIOR + 1;
-        
+
         for (int i = 0; i < 3; i ++) { /*  Genera las lineas de una barra  */
             
             switch (i) {
                 case 0: /*  Para crear primera linea  */
-                    x1 = (50 + 20) * j;
-                    y1 = 500;
                     
-                    x2 = (50 + 20) * j;
-                    y2 = 100 * xMax;
+                    barras.setX1((50 + 20) * j);
+                    barras.setY1(500);
+                    
+                    barras.setX2((50 + 20) * j);
+                    barras.setY2(100 * xMax);
+
                     break;
                 case 1: /*  Para crear linea interseptora */
-                    x1 = (50 + 20) * j;
-                    y1 = y2;
                     
-                    x2 = (50 + 20) * j + 50;
-                    y2 = 100 * xMax;
+                    barras.setX1((50 + 20) * j);
+                    barras.setY1(barras.getY2());
+                    
+                    barras.setX2((50 + 20) * j + 50);
+                    barras.setY2(100 * xMax);
+
                     break;
                 case 2: /*  Para crear segunda linea  */
-                    x1 = (50 + 20) * j + 50;
-                    y1 = y2;
                     
-                    x2 = (50 + 20) * j + 50;
-                    y2 = 500;
+                    barras.setX1((50 + 20) * j + 50);
+                    barras.setY1(barras.getY2());
+
+                    barras.setX2((50 + 20) * j + 50);
+                    barras.setY2(500);
+
                     break;
                 default:
                     break;
             }
             
-            /*  Dibuja la linea correspondiente con los parametros establecidos  */
-            gfx_line(x1, y1, x2, y2);
+            barras.graficarBarra();
+            
         }
     }
     
